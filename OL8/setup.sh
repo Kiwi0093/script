d8cc85d23e67100375f198fcb27717e4a5cc6de8
Kiwi0093 <49865887+Kiwi0093@users.noreply.github.com>
2022-03-14 18:10:36 +0800

    update

--- OL8/setup.sh
+++ OL8/setup.sh
@@ -108,61 +108,60 @@ echo -e "${color1}Using Kiwi's Private Repo to Configure${NC}"
 echo -e "${color2}Are You Kiwi himself?(Yes/No)${NC}"
 while :
 do
-		read KIWI
-		case $KIWI in
-			Yes)
-			    mkdir /home/$NUSER/.ssh
-			    mkdir /root/.ssh/
-			    echo -e "${color2}Please Paste your Private SSH key for Github\n${NC}"
-				read GITKEY
-				echo "${GITKEY} > /home/$NUSER/.ssh/id_rsa
-				echo "${GITKEY} > /root/.ssh/id_rsa
-				chown -R $NUSER /home/$NUSER
-				chmod 400 /home/$NUSER/.ssh/id_rsa
-				chmod 400 /root/.ssh/id_rsa
-				cd /home/$NUSER
-				git clone git@github.com:Kiwi0093/docker-compose.git
-				docker-compose -f /home/$NUSER/docker-compose/vps/common/portainer-agent/docker-compose.yml up -d
-				docker-compose -f /home/$NUSER/docker-compose/vps/common/watchtower/docker-compose.yml up -d
-				break
-				;;
-			No)
-			    echo -e "${color1}Add portainer/watchtower containers and put docker-compose.yml into /root/docker-compose/${NC}"
-			    # add more compose files
-				mkdir /root/docker-compose
-				# watchtower
-				echo -e "${color1}Create docker-compose file for watchtower & Portainer${NC}"
-				echo "version: '3'" > /root/docker-compose/docker-compose.yml
-				echo "services:" >> /root/docker-compose/docker-compose.yml
-				echo "  watchtower:" >> /root/docker-compose/docker-compose.yml
-				echo "      image: containrrr/watchtower" >> /root/docker-compose/docker-compose.yml
-				echo "      container_name: watchtower" >> /root/docker-compose/docker-compose.yml
-				echo "      volumes:" >> /root/docker-compose/docker-compose.yml
-				echo "        - /var/run/docker.sock:/var/run/docker.sock" >> /root/docker-compose/docker-compose.yml
-				echo "      environment:" >> /root/docker-compose/docker-compose.yml
-				echo "        - TZ=Asia/Taipei" >> /root/docker-compose/docker-compose.yml
-				echo "      restart: unless-stopped" >> /root/docker-compose/docker-compose.yml
-				echo '      command: --cleanup --schedule "0 0 4 * * *"' >> /root/docker-compose/docker-compose.yml
-				# Portainer
-				echo "" >> /root/docker-compose/docker-compose.yml
-				echo "  portainer:" >> /root/docker-compose/docker-compose.yml
-				echo "    image:  portainer/portainer-ce:latest" >> /root/docker-compose/docker-compose.yml
-				echo "    container_name: portainer" >> /root/docker-compose/docker-compose.yml
-				echo "    environment:" >> /root/docker-compose/docker-compose.yml
-				echo "      - PUID=1000" >> /root/docker-compose/docker-compose.yml
-				echo "    ports:" >> /root/docker-compose/docker-compose.yml
-				echo "      - 9000:9000" >> /root/docker-compose/docker-compose.yml
-				echo "    volumes:" >> /root/docker-compose/pdocker-compose.yml
-				echo "      - /var/run/docker.sock:/var/run/docker.sock" >> /root/docker-compose/docker-compose.yml
-				echo "      - /var/lib/docker/volumes/portainer/data:/data" >> /root/docker-compose/docker-compose.yml
-				echo "    restart: always" >> /root/docker-compose/docker-compose.yml
-				docker-compose -f /root/docker-compose/docker-compose.yml up -d
-				break
-				;;
-			*)
-				continue
-				;;
-		esac
+                read KIWI
+                case $KIWI in
+                        Yes)
+                            mkdir /home/$NUSER/.ssh
+                            mkdir /root/.ssh/
+                            echo -e "${color2}Please Paste your Private SSH key for Github\n${NC}"
+                                vi /root/.ssh/id_rsa
+                                cp /root/.ssh/id_rsa /home/$NUSER/.ssh/
+                                chown -R $NUSER /home/$NUSER
+                                chmod 400 /home/$NUSER/.ssh/id_rsa
+                                chmod 400 /root/.ssh/id_rsa
+                                cd /home/$NUSER
+                                git clone git@github.com:Kiwi0093/docker-compose.git
+                                docker-compose -f /home/$NUSER/docker-compose/vps/common/portainer-agent/docker-compose.yml up -d
+                                docker-compose -f /home/$NUSER/docker-compose/vps/common/watchtower/docker-compose.yml up -d
+                                break
+                                ;;
+                        No)
+                            echo -e "${color1}Add portainer/watchtower containers and put docker-compose.yml into /root/docker-compose/${NC}"
+                            # add more compose files
+                                mkdir /root/docker-compose
+                                # watchtower
+                                echo -e "${color1}Create docker-compose file for watchtower & Portainer${NC}"
+                                echo "version: '3'" > /root/docker-compose/docker-compose.yml
+                                echo "services:" >> /root/docker-compose/docker-compose.yml
+                                echo "  watchtower:" >> /root/docker-compose/docker-compose.yml
+                                echo "      image: containrrr/watchtower" >> /root/docker-compose/docker-compose.yml
+                                echo "      container_name: watchtower" >> /root/docker-compose/docker-compose.yml
+                                echo "      volumes:" >> /root/docker-compose/docker-compose.yml
+                                echo "        - /var/run/docker.sock:/var/run/docker.sock" >> /root/docker-compose/docker-compose.yml
+                                echo "      environment:" >> /root/docker-compose/docker-compose.yml
+                                echo "        - TZ=Asia/Taipei" >> /root/docker-compose/docker-compose.yml
+                                echo "      restart: unless-stopped" >> /root/docker-compose/docker-compose.yml
+                                echo '      command: --cleanup --schedule "0 0 4 * * *"' >> /root/docker-compose/docker-compose.yml
+                                # Portainer
+                                echo "" >> /root/docker-compose/docker-compose.yml
+                                echo "  portainer:" >> /root/docker-compose/docker-compose.yml
+                                echo "    image:  portainer/portainer-ce:latest" >> /root/docker-compose/docker-compose.yml
+                                echo "    container_name: portainer" >> /root/docker-compose/docker-compose.yml
+                                echo "    environment:" >> /root/docker-compose/docker-compose.yml
+                                echo "      - PUID=1000" >> /root/docker-compose/docker-compose.yml
+                                echo "    ports:" >> /root/docker-compose/docker-compose.yml
+                                echo "      - 9000:9000" >> /root/docker-compose/docker-compose.yml
+                                echo "    volumes:" >> /root/docker-compose/pdocker-compose.yml
+                                echo "      - /var/run/docker.sock:/var/run/docker.sock" >> /root/docker-compose/docker-compose.yml
+                                echo "      - /var/lib/docker/volumes/portainer/data:/data" >> /root/docker-compose/docker-compose.yml
+                                echo "    restart: always" >> /root/docker-compose/docker-compose.yml
+                                docker-compose -f /root/docker-compose/docker-compose.yml up -d
+                                break
+                                ;;
+                        *)
+                                continue
+                                ;;
+                esac
 done
 echo -e "${color4}.........................................................................................completed${NC}"
 
